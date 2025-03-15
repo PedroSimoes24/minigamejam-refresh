79c32e973aa078fe496371722e0b99b5c18f53dc
Diogo Lopes <diogo.fm.lopes4@gmail.com>
2025-03-15 22:10:35 +0000

    poiler bug fixed

--- Scripts/interaction_manager.gd
+++ Scripts/interaction_manager.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
-@onready var player = get_tree().current_scene.find_child("player")
+@onready var player = get_tree().current_scene.find_child("Player")
 @onready var label = $Label
 
 const base_text = "[E] to "
+const INTERACTION_DISTANCE = 30
 
 var active_areas = []
 var can_interact = true
 
+func _ready():
+	if player == null:
+		print("Player node not found!")
+		
 func register_area(area: InteractionArea):
 	active_areas.push_back(area)
 	
@@ -19,6 +24,11 @@ func unregister_area(area:InteractionArea):
 func _process(delta):
 	if active_areas.size() > 0 && can_interact:
 		active_areas.sort_custom(_sort_by_distance_to_player)
+		
+		if player.global_position.distance_to(active_areas[0].global_position) > INTERACTION_DISTANCE:
+			label.hide()
+			return
+		
 		label.text = base_text + active_areas[0].action_name
 		label.global_position = active_areas[0].global_position
 		label.global_position.y -= 36
@@ -28,6 +38,9 @@ func _process(delta):
 		label.hide()
 
 func _sort_by_distance_to_player(area1, area2):
+	if player == null:
+		return
+	
 	var area1_to_player = player.global_position.distance_to(area1.global_position)
 	var area2_to_player = player.global_position.distance_to(area2.global_position)
 	return area1_to_player < area2_to_player
